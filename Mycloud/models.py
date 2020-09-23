5ed7a7b445547ef3d92f3ccde0aff4854a6ae96f
emmywise <adefila.emmywise@gmail.com>
2020-09-23 12:25:36 +0100

    add doc models,fk delete

--- Mycloud/models.py
+++ Mycloud/models.py
@@ -2,6 +2,9 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.conf import settings
+
+
 # Create your models here.
 
 
@@ -13,9 +16,25 @@ class Profile(models.Model):
     # profile_pic = models.ImageField(upload_to='profile_pics',blank=True)
     def __str__(self):
         return self.user.username
+    
 @receiver(post_save, sender=User)
 def update_user_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
         instance.profile.save()
-    
+        
+def user_directory_path(instance, filename):
+    # file will be uploaded to MEDIA_ROOT/user_<id>/<filename>
+    return 'user_{0}/{1}'.format(instance.user.id, filename)
+
+class Document(models.Model):
+    description = models.CharField(max_length=255, blank=True)
+    document = models.FileField(upload_to='documents/%Y/%m/%d/')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
+    def __str__(self):
+        return self.description
+
+    def delete(self, *args, **kwargs):
+        self.document.delete()
+        super().delete(*args, **kwargs)
