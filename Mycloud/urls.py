0ebd6c54153586e675bf19a948c454a225dc02b9
emmywise <adefila.emmywise@gmail.com>
2020-09-23 12:43:10 +0100

    add upload and delete ulrs

--- Mycloud/urls.py
+++ Mycloud/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 from . import views
 
-
 app_name = 'Mycloud'
 urlpatterns = [
     path('', views.index, name='index'),
     path('signup/', views.signup, name='signup'), 
     path('login/', views.user_login, name='login'),
+    path('logout/', views.user_logout, name='logout'),
     path('home/', views.home, name='home'),
+    path('upload/', views.upload, name='upload'),
+    path('delete/<int:pk>/', views.delete_file, name='delete_file'),
+        
 ]
